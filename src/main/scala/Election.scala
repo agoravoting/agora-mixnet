6e5cdff9611f7f439f6f40af1d93e3288e486ac8
ruescasd <fastness@gmail.com>
2016-01-10 13:52:40 +0100

    added level 3 demo

--- src/main/scala/Election.scala
+++ src/main/scala/Election.scala
@@ -41,16 +41,13 @@ import ch.bfh.unicrypt.crypto.encoder.interfaces.Encoder
  * encoding of natural numbers to provide length-typed lists (aka dependent types), that way we get:
  *
  * 1) The election process logic is captured by types, so illegal transitions
- *    are caught by the compiler and inconsistent states are not possible
- *
- *    For example
+ *    are caught by the compiler and inconsistent states are not possible, for example
  *
  *    It is a compile-time error to try to construct the public key without all the shares
- *    It is a compile-time error to add more shares or shuffles than expected
+ *    It is a compile-time error to add more shares,shuffles or decryptions than expected
  *    It is a compile-error to start an election with no public key
  *    It is a compile-time error to decrypt without shuffling
  *    etc.
- *
  *
  * 2) Because the election is purely functional, the entire history of the election
  *    can be reconstructed or replayed. A purely functional data structure is in this
