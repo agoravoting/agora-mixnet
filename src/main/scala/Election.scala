a61124051b8eb2ee80b24d20685107d037b5f5b1
ruescasd <fastness@gmail.com>
2016-03-10 22:19:27 +0100

    split Util out

--- src/main/scala/Election.scala
+++ src/main/scala/Election.scala
@@ -266,63 +266,6 @@ trait HasHistory {
 }
 
 /**
- * Some utilities
- */
-object Util {
-  val unsafe = ConfigFactory.load().getBoolean("bypass-membership-check")
-  val generatorsParallel = ConfigFactory.load().getBoolean("use-generators-parallel")
-
-  def tupleFromSeq(items: Seq[Element[_]]) = {
-    // var tuple = Tuple.getInstance()
-    // items.foreach(v => tuple = tuple.add(v))
-
-    // tuple
-    Tuple.getInstance(items:_*)
-  }
-
-  def seqFromTuple(tuple: Tuple): Seq[Element[_]] = {
-
-    tuple.par.map{ x => x }.seq.toSeq
-  }
-
-  def getRandomVotes(size: Int, generator: Element[_], publicKey: Element[_]) = {
-    val elGamal = ElGamalEncryptionScheme.getInstance(generator)
-
-    (1 to size).map { _ =>
-      val element = elGamal.getMessageSpace().getRandomElement()
-      println(s"* plaintext $element")
-      elGamal.encrypt(publicKey, element)
-    }
-  }
-
-  def encryptVotes(plaintexts: Seq[Int], cSettings: CryptoSettings, publicKey: Element[_]) = {
-    val elGamal = ElGamalEncryptionScheme.getInstance(cSettings.generator)
-    val encoder = ZModPrimeToGStarModSafePrime.getInstance(cSettings.group)
-
-    plaintexts.par.map { p =>
-      val message = encoder.getDomain().getElementFrom(p)
-      val encodedMessage = encoder.encode(message)
-      elGamal.encrypt(publicKey, encodedMessage)
-    }.seq
-  }
-
-  def getPublicKeyFromString(publicKey: String, generator: Element[_]) = {
-    val elGamal = ElGamalEncryptionScheme.getInstance(generator)
-    val keyPairGen = elGamal.getKeyPairGenerator()
-    keyPairGen.getPublicKeySpace().getElementFrom(publicKey)
-  }
-
-  def getE[A <: Element[B],B](set: AbstractSet[A, B], value: String): Element[B] = {
-    set.getElementFromString(value, unsafe)
-  }
-
-  // we shouldnt have to have this in addition to the above method, but otherwise the compiler gets confused
-  def getE(set: ProductSet, value: String): Element[_] = {
-    set.getElementFromString(value, unsafe)
-  }
-}
-
-/**
  * Convenience election states used to carry information in the election history forward
  */
 class ElectionState(val id: String, val cSettings: CryptoSettings)
