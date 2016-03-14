f7d60b217633984be8fd5dec16f88b5942c9f39f
ruescasd <fastness@gmail.com>
2016-03-14 17:55:17 +0100

    shell

--- src/main/shell/run.sh
+++ src/main/shell/run.sh
@@ -7,7 +7,7 @@ USE_GENERATORS_PARALLEL=true
 GENERATORS_PARALLELISM_LEVEL=16
 CLUSTER_SEED=localhost:2555
 
-CPATH=../target/scala-2.11/sandbox_2.11-0.1-SNAPSHOT.jar:../target/scala-2.11/sandbox-assembly-0.1-SNAPSHOT-deps.jar
+CPATH=target/scala-2.11/sandbox_2.11-0.1-SNAPSHOT.jar:target/scala-2.11/sandbox-assembly-0.1-SNAPSHOT-deps.jar
 
 # GC_LOGGING="XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gc.log"
 OPTIONS="-Dmpservice.use-gmp=$USE_GMP -Dmpservice.use-extractor=$USE_EXTRACTOR -Dbypass-membership-check=BYPASS_MEMBERSHIP_CHECK -Duse-generators-parallel=USE_GENERATORS_PARALLEL -Dgenerators-parallelism-level=GENERATORS_PARALLELISM_LEVEL -Dakka.cluster.seed-nodes.0=akka.tcp://ClusterSystem@$CLUSTER_SEED"
--- src/main/shell/worker-run.sh
+++ src/main/shell/worker-run.sh
@@ -7,6 +7,6 @@ PORT=2555
 
 OPTIONS="-Dmpservice.use-gmp=$USE_GMP -Dakka.remote.netty.tcp.port=$PORT -Dakka.remote.netty.tcp.hostname=$HOSTNAME -Dakka.cluster.seed-nodes.0=akka.tcp://ClusterSystem@$CLUSTER_SEED"
 
-CPATH=../target/scala-2.11/sandbox_2.11-0.1-SNAPSHOT.jar:../target/scala-2.11/sandbox-assembly-0.1-SNAPSHOT-deps.jar
+CPATH=target/scala-2.11/sandbox_2.11-0.1-SNAPSHOT.jar:target/scala-2.11/sandbox-assembly-0.1-SNAPSHOT-deps.jar
 
 java $OPTIONS -d64 -Xms1G -Xmx1G -Djava.library.path=. -classpath $CPATH mpservice.WorkerApp $*
