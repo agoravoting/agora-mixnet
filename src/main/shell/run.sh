87a2e88285436b92fbe2c52de860bfeecea13369
ruescasd <fastness@gmail.com>
2016-03-15 23:26:36 +0100

    now using assembly whole jar

--- src/main/shell/run.sh
+++ src/main/shell/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#
+# this script must be run from the home directory, so first copy it there
+#
+
 USE_GMP=true
 USE_EXTRACTOR=true
 BYPASS_MEMBERSHIP_CHECK=true
@@ -7,7 +11,8 @@ USE_GENERATORS_PARALLEL=true
 GENERATORS_PARALLELISM_LEVEL=16
 CLUSTER_SEED=localhost:2555
 
-CPATH=target/scala-2.11/sandbox_2.11-0.1-SNAPSHOT.jar:target/scala-2.11/sandbox-assembly-0.1-SNAPSHOT-deps.jar
+# you must have run the assembly command from sbt for this to work
+CPATH=target/scala-2.11/sandbox-assembly-0.1-SNAPSHOT.jar
 
 # GC_LOGGING="XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gc.log"
 OPTIONS="-Dmpservice.use-gmp=$USE_GMP -Dmpservice.use-extractor=$USE_EXTRACTOR -Dbypass-membership-check=$BYPASS_MEMBERSHIP_CHECK -Duse-generators-parallel=$USE_GENERATORS_PARALLEL -Dgenerators-parallelism-level=$GENERATORS_PARALLELISM_LEVEL -Dakka.cluster.seed-nodes.0=akka.tcp://ClusterSystem@$CLUSTER_SEED"
--- src/main/shell/worker-run.sh
+++ src/main/shell/worker-run.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+#
+# this script must be run from the home directory, so first copy it there
+#
+
 USE_GMP=true
 CLUSTER_SEED=localhost:2555
 HOSTNAME=localhost
 PORT=2555
 
-OPTIONS="-Dmpservice.use-gmp=$USE_GMP -Dakka.remote.netty.tcp.port=$PORT -Dakka.remote.netty.tcp.hostname=$HOSTNAME -Dakka.cluster.seed-nodes.0=akka.tcp://ClusterSystem@$CLUSTER_SEED"
+# you must have run the assembly command from sbt for this to work
+CPATH=target/scala-2.11/sandbox-assembly-0.1-SNAPSHOT.jar
 
-CPATH=target/scala-2.11/sandbox_2.11-0.1-SNAPSHOT.jar:target/scala-2.11/sandbox-assembly-0.1-SNAPSHOT-deps.jar
+OPTIONS="-Dmpservice.use-gmp=$USE_GMP -Dakka.remote.netty.tcp.port=$PORT -Dakka.remote.netty.tcp.hostname=$HOSTNAME -Dakka.cluster.seed-nodes.0=akka.tcp://ClusterSystem@$CLUSTER_SEED"
 
 java $OPTIONS -d64 -Xms1G -Xmx1G -Djava.library.path=. -classpath $CPATH mpservice.WorkerApp $*
